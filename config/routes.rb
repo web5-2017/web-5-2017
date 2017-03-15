c6349310fe4cec2c19f1552b4ca8c6ceb2a2c2a3
Jefferson Henrique Ramos <jeff.hen0811@gmail.com>
2017-03-14 23:18:16 -0300

    config gemfile

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root to: "home#index"
 
-  get to: "application#hello"
+  get '/hello', to: "application#hello"
 
-  get to: "application#goodbye"
+  get '/goodbye', to: "application#goodbye"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
