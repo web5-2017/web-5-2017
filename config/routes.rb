987a2bef2d3ac77f9db48ee60e8d5fed24105a15
Jefferson Henrique Ramos <jeff.hen0811@gmail.com>
2017-05-02 18:14:31 -0300

    add exercise 8

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
 
   get '/goodbye', to: "application#goodbye"
 
+  get '/exercises-8-basic_login', to: 'exercises#basic_login'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
