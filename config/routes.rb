1cc8a98df0c89c5f51e2f0edb807b1592f6c0594
Jefferson Henrique Ramos <jeff.hen0811@gmail.com>
2017-06-06 23:19:31 -0300

    exercises 13

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
   get '/exercises-8-basic_login', to: 'exercises#basic_login'
   get '/exercises-9-advanced_login', to: 'exercises#advanced_login'
+  get '/exercises-13-user_microposts', to: 'exercises#user_microposts'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
